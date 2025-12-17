4c5d5179472d2f10c239145c3e57549d95672541
gituser708 <donmania999@gmail.com>
2025-12-17 18:28:25 +0530

    updated email version in middlewares

--- Node/middlewares/welcomeEmail.js
+++ Node/middlewares/welcomeEmail.js
@@ -2,7 +2,9 @@ require('dotenv').config({ quiet: true });
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
